f48b587ea3d05453552881ef7bb665515f97c238
sun <1003008727@qq.com>
2018-01-26 22:41:47 +0800

    添加session管理

--- NetWorkManage/views.py
+++ NetWorkManage/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.sessions.models import Session
 from NetWorkManage import models
+
 
 # Create your views here.
 
 
 def Login(request):
-    if request.method=='GET':
-        return render(request,'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
     else:
-        name=request.POST.get('Email')
-        pwd=request.POST.get('Password')
-        obj = models.User.objects.filter(UserEmail=name,UserPassword=pwd).first()
+        name = request.POST.get('Email')
+        pwd = request.POST.get('Password')
+        obj = models.User.objects.filter(UserEmail=name, UserPassword=pwd).first()
         if obj:
-            print(obj)
-            #1、生成随机字符串（sessionID）
-            #2、通过cookie发送给客户端
-            #3、服务端保存{zhanggen随机字符串:{'name':'zhanggen'.'email':'zhanggen@le.com'}}
-            request.session['email']=obj.UserEmail #在Django 中一句话搞定
+            # 1、生成随机字符串（sessionID）
+            # 2、通过cookie发送给客户端
+            # 3、服务端保存{zhanggen随机字符串:{'name':'zhanggen'.'email':'zhanggen@le.com'}}
+            request.session['email'] = obj.UserEmail  # 在Django 中一句话搞定
             return redirect('/index/')
         else:
-            return render(request,'login.html',{'info':"用户名/密码错误"})
+            return render(request, 'login.html', {'info': "用户名/密码错误"})
+
 
 def Index(request):
     v = request.session.get('email')
+    # vv=Session.objects.get(pk='ku3im28nmzqfimwevwhmhfi4970fc2qo')
+    # print(vv.get_decoded())
     if v:
         return render(request, 'index.html', {'email': v})
     else:
@@ -33,5 +37,9 @@ def Index(request):
 def LogOut(request):
     v = request.session.get('email')
     if v:
-        request.session.delete('email')
+        print(v+"退出")
+        try:
+            del request.session['email']
+        except:
+            print("删除session错误")
         return redirect('/login/')
