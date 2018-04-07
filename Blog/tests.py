16b3c6f0dc4a8cd67caef36f06b54739a77d1aa4
sun <1003008727@qq.com>
2018-04-07 12:06:50 +0800

    提交一下

--- Blog/tests.py
+++ Blog/tests.py
@@ -1,6 +1,4 @@
-from django.test import TestCase
 
-# Create your tests here.
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     return "Hello World"
